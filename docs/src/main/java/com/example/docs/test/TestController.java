c27a66ab03f302cc79a238f4fb3471e3879d6c45
ingyu1008 <ingyu1008@gmail.com>
2021-06-26 03:35:56 +0900

    UPD : constant encaptulation & individual query

--- docs/src/main/java/com/example/docs/test/TestController.java
+++ docs/src/main/java/com/example/docs/test/TestController.java
@@ -1,6 +1,10 @@
 package com.example.docs.test;
 
+import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +19,15 @@ public class TestController {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Value("${secure.key}")
+    private Integer key;
+
+    @Value("${urls.level}")
+    private String level;
+
     @PostMapping("/add")
-    public @ResponseBody String addNewUser(@RequestBody Member body, @RequestParam String pass) {
-        if (pass.equals("2020011776")) {
+    public @ResponseBody String addNewUser(@RequestBody Member body, @RequestParam Integer pass) {
+        if (pass.intValue() == key.intValue()) {
             // return body.getHandle();
             memberRepository.save(body);
             return "Done!";
@@ -33,5 +43,36 @@ public class TestController {
     @GetMapping("/team")
     public Iterable<Member> userList(@RequestParam Integer team) {
         return memberRepository.findByTeam(team);
+    }
+
+    // @GetMapping("/level/organization")
+    // public Iterable<Level> levelStats(@RequestParam Integer organizationId) {
+    // Iterable<Member> arr = memberRepository.findByTeam(organizationId);
+        /*
+        *  TODO : search by team
+        * 
+        */
+    // }
+
+    @GetMapping("/level/individual")
+    public Level levelStats(@RequestParam String handle, @RequestParam Integer tier) {
+        JSONObject res;
+        String url = level;
+        Level result = new Level();
+        if (tier < 0 || tier > 30)
+            return result;
+        result.setLevel(tier);
+
+        res = RestAPICaller.restCall(url + "tier:" + tier);
+        Integer count = Integer.parseInt(res.get("count").toString());
+        result.setCount(count);
+
+        res = RestAPICaller.restCall(url + "tier:" + tier + "%20solved_by:" + handle);
+        Integer solved = Integer.parseInt(res.get("count").toString());
+        result.setSolved(solved);
+        result.setUnsolved(count - solved);
+        result.setPercentage(solved * 1.0 / count);
+
+        return result;
     }
 }
