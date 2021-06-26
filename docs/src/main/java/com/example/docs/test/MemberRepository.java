3ac337f2882fb9ecc76ee911e18d03664cf73355
ingyu1008 <ingyu1008@gmail.com>
2021-06-27 01:28:26 +0900

    UPD : change query parameterization

--- docs/src/main/java/com/example/docs/test/MemberRepository.java
+++ docs/src/main/java/com/example/docs/test/MemberRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends CrudRepository<Member, Integer> {
 
-    @Query(value = "select * from member where team = ?1", nativeQuery = true)
-    List<Member> findByTeam(Integer team);
+    @Query(value = "select * from member where team = :team", nativeQuery = true)
+    List<Member> findByTeam(@Param("team") Integer team);
 
     @Query(value = "select * from member", nativeQuery = true)
     List<Member> findAll();
