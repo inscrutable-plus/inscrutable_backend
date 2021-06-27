54dbe39537eebf7ebdc739ef7de94da8d90d4a88
ingyu1008 <ingyu1008@gmail.com>
2021-06-28 00:26:36 +0900

    UPD : add @repository annotation

--- docs/src/main/java/com/example/docs/test/ProblemRepository.java
+++ docs/src/main/java/com/example/docs/test/ProblemRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProblemRepository extends CrudRepository<Problem, Integer> {
 
     @Query(value = "select * from problems where title like concat('%',:title,'%') limit 10", nativeQuery = true)
