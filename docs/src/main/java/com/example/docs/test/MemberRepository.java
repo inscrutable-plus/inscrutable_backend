42607efcde71c5fd4e7b9d579e9188196c645d9c
ingyu1008 <ingyu1008@gmail.com>
2021-07-04 16:35:46 +0900

    UPD : add handle query

--- docs/src/main/java/com/example/docs/test/MemberRepository.java
+++ docs/src/main/java/com/example/docs/test/MemberRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,4 +15,7 @@ public interface MemberRepository extends CrudRepository<Member, Integer> {
 
     @Query(value = "select * from member", nativeQuery = true)
     List<Member> findAll();
+
+    @Query(value = "select id from member where handle = :handle limit 1", nativeQuery = true)
+    Integer findByHandle(@Param("handle") String handle);
 }
