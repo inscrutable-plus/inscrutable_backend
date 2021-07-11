5f73e2decda27aa85e4ede552eb2ccf150e7e99f
ingyu1008 <ingyu1008@gmail.com>
2021-07-11 21:24:59 +0900

    UPD: add few more queries to solveRepository findSolveListBy(Handle/Id) findSolveListByIdAndDate findScoresByTime

--- docs/src/main/java/com/example/docs/test/SolveRepository.java
+++ docs/src/main/java/com/example/docs/test/SolveRepository.java
@@ -47,6 +47,18 @@ public interface SolveRepository extends CrudRepository<Solve, SolvePrimaryKey> 
     @Query(value = "select * from solve where id = :id and problem_id = :problem_id", nativeQuery = true)
     List<Map<String, Object>> findByIds(@Param("id") Integer id, @Param("problem_id") Integer problemId);
 
+    @Query(value = "select member.handle, solve.problem_id, problem.title, problem.level, solve.solve_date from solve join member on member.id = solve.id join problem on problem.problem_id = solve.problem_id where member.handle = :handle", nativeQuery = true)
+    List<Map<String, Object>> findSolveListByHandle(@Param("handle") String handle);
+
+    @Query(value = "select problem.title, solve.* from solve join problem on solve.problem_id = problem.problem_id where solve.id = :id order by solve.solve_date asc", nativeQuery = true)
+    List<Map<String, Object>> findSolveListById(@Param("id") Integer id);
+
+    @Query(value = "select problem.title, problem.level, solve.* from solve join problem on solve.problem_id = problem.problem_id where solve.id = :id and solve.solve_date >= :start and solve.solve_date < :end order by solve.solve_date asc", nativeQuery = true)
+    List<Map<String, Object>> findSolveListByIdAndDate(@Param("id") Integer id, @Param("start") Timestamp start, @Param("end") Timestamp end);
+
+    @Query(value =  "select solve.id, sum(problem.level) as score from solve join problem on solve.problem_id = problem.problem_id where solve.solve_date >= :start and solve.solve_date < :end group by solve.id", nativeQuery = true)
+    List<Map<String, Object>> findScoresByTime(@Param("start") Timestamp start, @Param("end") Timestamp end);
+
     @Transactional
     @Modifying
     @Query(value = "insert into solve(problem_id, id, solve_date) values(:problem_id, :id, :solve_date)", nativeQuery = true)
